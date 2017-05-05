8c204d360ce0c8d1c88849d9e388a937ce177801
gom-desktop <wangqw920@noreply.git.com>
2017-05-06 01:53:01 +0800

    fix connect internet, disable use system browes

--- app/src/main/java/com/gom/tools/mytools/browes/Activity_browes.java
+++ app/src/main/java/com/gom/tools/mytools/browes/Activity_browes.java
@@ -1,10 +1,12 @@
 package com.gom.tools.mytools.browes;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import com.gom.tools.mytools.R;
@@ -14,6 +16,7 @@ public class Activity_browes extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_browes);
          wv= (WebView) findViewById(R.id.webview_browees);
         WebSettings websettings=wv.getSettings();
@@ -21,6 +24,14 @@ public class Activity_browes extends AppCompatActivity {
         websettings.setBuiltInZoomControls(true);
         websettings.setLoadsImagesAutomatically(false);
         websettings.setJavaScriptEnabled(true);
+        //websettings.setAppCacheEnabled(true);
+        if (Build.VERSION.SDK_INT >= 19) {
+            websettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        }
+
+        //disable use system browes
+        wv.setWebViewClient(new WebViewClient());
+
     }
 
     public void onclick_openUrl(View view){
