358c48fbecc68d0a2b34f6707b8be810c94adc2c
gom-mgwpc <noreply@github.com>
2017-05-06 19:32:28 +0800

    add back and forward add show URL max page

---
+++ app/src/main/java/com/gom/tools/mytools/Activity_Battery.java
@@ -0,0 +1,15 @@
+package com.gom.tools.mytools;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Battery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__battery);
+    }
+
+
+}
--- app/src/main/java/com/gom/tools/mytools/browes/Activity_browes.java
+++ app/src/main/java/com/gom/tools/mytools/browes/Activity_browes.java
@@ -13,31 +13,58 @@ import com.gom.tools.mytools.R;
 
 public class Activity_browes extends AppCompatActivity {
     private WebView wv;
+    private TextView tv ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_browes);
-         wv= (WebView) findViewById(R.id.webview_browees);
+        tv= (TextView) findViewById(R.id.text_url);
+        wv= (WebView) findViewById(R.id.webview_browees);
         WebSettings websettings=wv.getSettings();
         websettings.setSupportZoom(true);
         websettings.setBuiltInZoomControls(true);
         websettings.setLoadsImagesAutomatically(false);
         websettings.setJavaScriptEnabled(true);
+        websettings.setDomStorageEnabled(true);
         //websettings.setAppCacheEnabled(true);
         if (Build.VERSION.SDK_INT >= 19) {
             websettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
         }
 
         //disable use system browes
-        wv.setWebViewClient(new WebViewClient());
+        wv.setWebViewClient(new WebViewClient()
+                            {
+                                @Override
+                                public void onPageFinished(WebView view, String url) {
+                                   tv.setText(wv.getUrl());
+                                }
+                            }
+        );
 
     }
 
     public void onclick_openUrl(View view){
+    //  move to top
+        tv= (TextView) findViewById(R.id.text_url) ;
+        String string_url = tv.getText().toString();
+        if(string_url.isEmpty()){
+            wv.loadUrl("https://www.baidu.com");
+        }else {
+            wv.loadUrl(string_url);
+        }
 
-        TextView tv= (TextView) findViewById(R.id.text_url) ;
-        wv.loadUrl(tv.getText().toString());
     }
+
+    public void onclick_goForward(View view){
+        wv.goForward();
+    }
+
+    @Override
+    public void onBackPressed(){
+        wv.goBack();
+    }
+
+
 
 }
